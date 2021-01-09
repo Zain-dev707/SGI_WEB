0f83a3eb75846ef9183e25109490cbe194a7be66
Tauqeer <abdulrehman@ewdtech.com>
2021-01-09 18:48:52 +0500

    fixes

--- Assets/js/dropdown.js
+++ Assets/js/dropdown.js
@@ -99,14 +99,7 @@ function populatevalue(cursoelement) {
 
         "fundamental11": "FUNDAMENTAL II"
     },]
-    let MÉDIO = [{
 
-        "Medio": "MÉDIO"
-    },
-    {
-
-        "fundamental11": "FUNDAMENTAL II"
-    },]
     var fundamentalelement = document.getElementById('sel-3');
     let allelement = "";
 
